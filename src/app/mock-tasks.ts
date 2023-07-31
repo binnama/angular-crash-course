a3c0a770d4517d43ef47c7eb90486d8c94975a21
binnama <laasestad@yahoo.no>
2023-07-31 20:06:57 +0200

    Added a add-function

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[]= [
+    {
+        id: 1,
+        text: "Publish a book",
+        day: "Dec 10th at 12:00pm",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Go hunting",
+        day: "Aug 25th at 5:00am",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Watch a marathon",
+        day: "Sept 16th at 12:00pm",
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: "Feed dog",
+        day: "Aug 10th at 8:00am",
+        reminder: false,
+    },
+];
